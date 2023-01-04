962ca842d60ff0b390c851582ec41a0cfe82dd50
Chris Stekas <christost20@gmail.com>
2023-01-04 17:39:07 +0200

    Added scenario for invalid nucleotide bases and added in-code comments

--- script.pl
+++ script.pl
@@ -3,11 +3,17 @@
 
 while ( $line = <STDIN> )
 {
-	if ( $line =~ /(ATG(...)*?(TAA|TAG|TGA))/ )
-                 { print("Found the ORF sequence $1 in $line \n"); }
+	#Return an error for invalid (non-ATGC nucleotide) bases
+	 if ( $line =~ /[^ATGC\n]/ ) 
+		{ print ("Invalid sequence: Sequence should only contain A,T,C,G \n"); 
+		  next; 
+		}
+
+	#Search for the shortest ORF sequence(so that there are no stop codons between the sequence)
+	elsif ( $line =~ /(ATG(...)*?(TAA|TAG|TGA))/ )
+                 { print ("Found the ORF sequence $1 in $line \n"); }
 	else
-		 { print("Could not find an ORF sequence \n");
+		 { print ("Could not find an ORF sequence \n");
 	           next;	
 		 }
-
 }
